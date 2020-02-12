e7ed3de9aaf2776c0c19c2d5bd87d16ec6a3c399
Nahid fatima <fatimanahid04@gmail.com>
2020-02-12 12:35:17 +0530

    #Nahid Fatima: defining anonymous function

---
+++ main.js
@@ -0,0 +1,44 @@
+
+//Anonymous Function
+
+(
+    (
+        function(){
+         console.log("Anonymous Function");
+        }
+
+    )
+)();
+
+
+let result =(
+    (
+       function(strName){
+         return  strName;
+
+       }
+       
+    )
+)("Nahid Fatima");
+  console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
